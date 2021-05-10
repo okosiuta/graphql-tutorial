8509df989d3d8ae7fe926d73ac1a629c10d75ecb
okosiuta <okosiuta@xybion.com>
2021-05-10 15:51:41 +0300

    dev: updated publisher configs

--- src/main/java/com/tutorial/graphql/graphqltutorial/publisher/impl/AuthorPublisher.java
+++ src/main/java/com/tutorial/graphql/graphqltutorial/publisher/impl/AuthorPublisher.java
@@ -5,10 +5,12 @@ import com.tutorial.graphql.graphqltutorial.publisher.AbstractPublisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Sinks;
 
+import static reactor.util.concurrent.Queues.SMALL_BUFFER_SIZE;
+
 @Component
 public class AuthorPublisher extends AbstractPublisher<Author> {
 
     public AuthorPublisher() {
-        super(Sinks.many().multicast().onBackpressureBuffer());
+        super(Sinks.many().multicast().onBackpressureBuffer(SMALL_BUFFER_SIZE, false));
     }
 }
--- src/main/java/com/tutorial/graphql/graphqltutorial/publisher/impl/ReviewPublisher.java
+++ src/main/java/com/tutorial/graphql/graphqltutorial/publisher/impl/ReviewPublisher.java
@@ -5,10 +5,12 @@ import com.tutorial.graphql.graphqltutorial.publisher.AbstractPublisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Sinks;
 
+import static reactor.util.concurrent.Queues.SMALL_BUFFER_SIZE;
+
 @Component
 public class ReviewPublisher extends AbstractPublisher<Review> {
 
     public ReviewPublisher() {
-        super(Sinks.many().multicast().onBackpressureBuffer());
+        super(Sinks.many().multicast().onBackpressureBuffer(SMALL_BUFFER_SIZE, false));
     }
 }
